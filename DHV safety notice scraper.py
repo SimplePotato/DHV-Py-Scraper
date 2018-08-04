b2677cd977e119ae6c8c9bb355fb777533b4cc34
SimplePotato <domen.kastner@gmail.com>
2018-08-04 12:26:58 +0200

    Added loop for categories, remove functions, new names for clearer code

--- DHV safety notice scraper.py
+++ DHV safety notice scraper.py
@@ -7,7 +7,8 @@ from datetime import datetime as dt
     #Now Date
 today = dt.now()
 date_string = dt.strftime(today, '%m.%d.%Y') #the date is in the same format as the date in a ckass="dhv_sm data datum"
-print(date_string) 
+print('\n' + date_string + '\n') 
+print('\n'+"----------------"+'\n')
 
     #Source HTML Code
 '''<tr>
@@ -16,23 +17,32 @@ print(date_string)
 	<td class="dhv_sm data bezug"><a href="/db1/source/technicdatareportnotes.php?lang=en&amp;item=267" class="dhv_sm bezug" target="_blank">Paraglider Harness Progress 3</a></td>
    </tr>
 ''' 
+
     #Source  
 html = urlopen("https://www.dhv.de/en/safety/safety-notes/")
 soup = BeautifulSoup(html, "lxml")
-    #Get last released note as item name
 
-sNotice = soup.find_all("td", class_="dhv_sm data bezug")
-for sNotice in sNotice:
-    print(sNotice.text)
+    #get category of a notice
+kat = soup.find_all("td", class_="kategorie")
+for kategorie in kat:
+    print(kategorie.text)
 
-    #Here I get a list of items successfully, but I don't know how to implement them into sNoticeAddText() function under the name "item". Every item should be with their own link ???
+    #I get a list of category successfully, but I don't know how to implement them into final print???
+
+print('\n'+"----------------"+'\n')
+
+    #Get product name
+productName = soup.find_all("td", "a", class_="bezug")
+for product in productName:
+    print(product.text)
+
+    #I get a list of items successfully, but I don't know how to implement them into final print???
 
 print('\n'+"----------------"+'\n')
 
     #Get source link and add aditional text + "item"
-def sNoticeAddText(): 
-    sNoticeLink = soup.find_all('a', {'class': 'dhv_sm bezug', 'href': True})
-    for sNoticeLink in sNoticeLink:
-        #print('DHV releases IMPORTANT SAFETY notice about' + '  ' + item + '\n' 'To read more information about this IMPORTANT safety notice, click on the following link' + '\n' '\n' 'https://www.dhv.de/' + sNoticeLink['href'] + '\n' '\n' '\n')
-        print(date_string + ' DHV releases IMPORTANT SAFETY notice about' + '  ' + '????? item ?????' + '\n' 'To read more information about this IMPORTANT safety notice, click on the following link' + '\n' '\n' 'https://www.dhv.de/' + sNoticeLink['href'] + '\n' '\n' '\n')
-sNoticeAddText()
+safetyNoticeLink = soup.find_all('a', {'class': 'dhv_sm bezug', 'href': True})
+for link in safetyNoticeLink:
+    #print('DHV releases IMPORTANT SAFETY notice about' + '  ' + item + '\n' 'To read more information about this IMPORTANT safety notice, click on the following link' + '\n' '\n' 'https://www.dhv.de/' + sNoticeLink['href'] + '\n' '\n' '\n')
+    print('https://www.dhv.de/' + link['href'])
+    
